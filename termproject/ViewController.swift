0a65356381f8e7c1ecff6d2e6a615e7f4cfc3fdf
Bogeun Yu <qhrms97@gmail.com>
2022-05-29 12:57:34 +0900

    First Commit

--- termproject/ViewController.swift
+++ termproject/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 스토리보드 연결
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    let thisDate = DateFormatter();
+    
     override func viewDidLoad() {
+        // 년-월별 표시 날짜 포멧팅
+        thisDate.dateFormat = "yyyy-MM"
+        let current_date_string = thisDate.string(from: Date())
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        /* Start code*/
+        // 디폴트 날짜 지정
+        dateLabel.text = current_date_string
     }
     
-
+    @IBAction func right_click(_ sender: Any) {
+        print("Click RIGHT")
+    }
+    @IBAction func left_click(_ sender: Any) {
+        print("Click Left")
+    }
 }
 
