289d8d555cfc866eada7abc90ace8418735c62af
Bogeun Yu <qhrms97@gmail.com>
2022-05-29 12:23:06 +0900

    Initial Commit

---
+++ termproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  termproject
+//
+//  Created by 배유진 on 2022/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
