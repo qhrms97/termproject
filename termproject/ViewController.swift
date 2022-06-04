293711714cf858e4bb93f765d2700a0e597ed0bc
Bogeun Yu <qhrms97@gmail.com>
2022-06-04 15:24:00 +0900

    2022/06/04 Date Change SS

--- termproject/ViewController.swift
+++ termproject/ViewController.swift
@@ -12,26 +12,44 @@ class ViewController: UIViewController {
     // 스토리보드 연결
     @IBOutlet weak var dateLabel: UILabel!
     
-    let thisDate = DateFormatter();
+    let now = Date()
+    let thisDate = DateFormatter()
+    
+    var cal = Calendar.current
+    var components = DateComponents()
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        self.initView()
+    }
+    
+    private func initView(){
         // 년-월별 표시 날짜 포멧팅
         thisDate.dateFormat = "yyyy-MM"
-        let current_date_string = thisDate.string(from: Date())
         
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        components.year = cal.component(.year, from: now)
+        components.month = cal.component(.month, from: now)
+        components.day = 1
         
         /* Start code*/
         // 디폴트 날짜 지정
-        dateLabel.text = current_date_string
+        self.DateCollection()
     }
     
     @IBAction func right_click(_ sender: Any) {
-        print("Click RIGHT")
+        components.month = components.month! + 1
+        self.DateCollection()
     }
     @IBAction func left_click(_ sender: Any) {
-        print("Click Left")
+        components.month = components.month! - 1
+        self.DateCollection()
+    }
+    
+    // 날짜 변동 함수...? 따로 만들어야함
+    func DateCollection(){
+        let myDate = cal.date(from: components)
+        // dateLabel.text = current_date_string
+        dateLabel.text = thisDate.string(from: myDate!)
     }
 }
 
