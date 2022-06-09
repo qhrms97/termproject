6f594c6821da2e157a990daf532660517cd78ca1
Bogeun Yu <qhrms97@gmail.com>
2022-06-10 00:52:18 +0900

    FSCalendar 추가 및 날짜 추가 시 라벨에 뿌려줌

---
+++ termproject/SecondViewController.swift
@@ -0,0 +1,54 @@
+
+import UIKit
+import FSCalendar
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 달력의 평일 날짜 색깔
+        calendar.appearance.titleDefaultColor = .black
+
+        // 달력의 토,일 날짜 색깔
+        calendar.appearance.titleWeekendColor = .red
+
+        // 달력의 맨 위의 년도, 월의 색깔
+        calendar.appearance.headerTitleColor = .systemPink
+
+        // 달력의 요일 글자 색깔
+        calendar.appearance.weekdayTextColor = .orange
+        
+        // 달력의 년월 글자 바꾸기
+        calendar.appearance.headerDateFormat = "YYYY년 M월"
+
+        // 달력의 요일 글자 바꾸는 방법 1
+        calendar.locale = Locale(identifier: "ko_KR")
+                
+        // 달력의 요일 글자 바꾸는 방법 2
+        calendar.calendarWeekdayView.weekdayLabels[0].text = "일"
+        calendar.calendarWeekdayView.weekdayLabels[1].text = "월"
+        calendar.calendarWeekdayView.weekdayLabels[2].text = "화"
+        calendar.calendarWeekdayView.weekdayLabels[3].text = "수"
+        calendar.calendarWeekdayView.weekdayLabels[4].text = "목"
+        calendar.calendarWeekdayView.weekdayLabels[5].text = "금"
+        calendar.calendarWeekdayView.weekdayLabels[6].text = "토"
+        
+        // 년월에 흐릿하게 보이는 애들 없애기
+        calendar.appearance.headerMinimumDissolvedAlpha = 0
+        
+        // 터치 이벤트
+        calendar.delegate = self
+    }
+}
+
+extension SecondViewController: FSCalendarDelegate {
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateLabel.text = dateFormatter.string(from: date)
+    }
+}
